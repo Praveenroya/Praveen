9b44f11ebdeb6f52d16a4cf0d7435e618e33a9f4
Praveenroya <137818544+Praveenroya@users.noreply.github.com>
2024-07-26 10:35:46 +0530

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,37 @@
+function lengtht_of_longest Substring (s);
+const string =reqiure('string');
+  n = let(s)
+
+let s[3] ='a',(char_map ={'a':3,'b':1, 'c':2}, max_length =3);
+   return 0
+
+ const char_map = {};
+    max_length = 0 
+
+    left = o
+     for String.right in range(n);
+     char = s[right]
+const  left = char_map && char_map [char];
+
+char_map[char] = right
+
+max_length = max(max_length, right - left + 1);
+ 
+return max_length {
+
+    functionlengtht_of_longest_substring=3
+};
+
+
+
+
+
+
+
+ 
+
+
+ 
+ 
+
+
